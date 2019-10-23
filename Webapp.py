dd9e9b27a55769273003a2be1bc27b57636d938b
pchla24 <janusz_sawicki97@o2.pl>
2019-10-23 19:19:24 +0200

    App details added

--- Webapp.py
+++ Webapp.py
@@ -1,9 +1,9 @@
 from flask import Flask
-from flask import request
+from flask import request, render_template
 import requests
 import json
 
-from Mod import Downloader, Launcher, AppInfo, CTManager
+from .Mod import Downloader, Launcher, AppInfo, CTManager
 
 app = Flask(__name__)
 
@@ -14,7 +14,7 @@ path = "/library/launcher/applications"
 UserID = 123
 Username = "TestName"
 
-AppInfo = {
+""" AppInfo = {
     "id": 1234,
     "name": "SuperApp1",
     "description": "This app allows multiplying large matrices",
@@ -26,7 +26,7 @@ formInfo = {
     "param1": "xxx",
     "param2": "yyy",
     "logger": "loggertest"
-}
+} """
 
 
 @app.route('/launcher/app-user/applications')
@@ -41,3 +41,19 @@ def showApps():
 @app.route('/launcher/app-user/computations', methods=['POST'])
 def handleCT():
     launcher.postComputations(request.form)
+
+
+##################----EXPERIMENTS----####################
+
+
+@app.route('/launcher/app-user/application/<int:app_id>')
+def showAppDetails(app_id):
+    #TODO currentAppInfo = launcher.UserApps where id=app_id
+    #Example currnetAppInfo
+    currentAppInfo = AppInfo(123, 'Awful App', 'Really awful application', 'noicon')
+    return render_template('appDetails.html', appID=currentAppInfo.id, appName=currentAppInfo.name, 
+                                        appDescription=currentAppInfo.description, appIcon=currentAppInfo.icon)
+
+@app.route('/launcher/app-user/<int:app_id>/createComputationTask')
+def showComputationInputForm(app_id):
+    #TODO downloading form schema based on app_id
