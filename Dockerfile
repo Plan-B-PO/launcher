658546df3e81a1099d13bb3364b6d85d6052d12c
Bartek <cetlinskib@gmail.com>
2019-10-30 09:11:35 +0100

    Project files moved to new folders. Project more is more Dockerfile friendly now.

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,4 @@ WORKDIR /launcher
 COPY ./src /launcher
 RUN pip install --trusted-host pypi.python.orh -r requirements.txt
 EXPOSE 80
-CMD ["python", "Webapp.py"]
+CMD ["python", ".py"]
--- src/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from flask_mongoalchemy import MongoAlchemy
-
-app = Flask(__name__)
-app.config['MONGOALCHEMY_DATABASE'] = 'CTDatabase'
-db = MongoAlchemy(app)
-
-from src import Webapp
