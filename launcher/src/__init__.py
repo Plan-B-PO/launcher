fc2b70dfe787c0b023cd27a9bd27349d8687e3d3
Bartek <cetlinskib@gmail.com>
2019-11-19 20:48:26 +0100

    Changes in initialization.

--- launcher/src/__init__.py
+++ launcher/src/__init__.py
@@ -1,15 +1,10 @@
 from flask import Flask
 from flask_mongoalchemy import MongoAlchemy
-import pymongo
-from pymongo import MongoClient
-#mongo client is connected
-client = MongoClient()
-db     = client['db']
 
 app = Flask(__name__)
-#app.config['MONGOALCHEMY_DATABASE'] = 'CTDatabase' #'mongodb://127.0.0.1:27017/data/db/CTDatabase'
+app.config['MONGOALCHEMY_DATABASE'] = 'CTDatabase' #'mongodb://127.0.0.1:27017/data/db/CTDatabase'
 #app.config['MONGOALCHEMY_SERVER'] = 'mongo'
 #app.config['MONGOALCHEMY_PORT'] = '27017'
-#db = MongoAlchemy(app)
+db = MongoAlchemy(app)
 
 from launcher.src import Webapp
