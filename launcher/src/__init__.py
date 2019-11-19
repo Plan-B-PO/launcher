5100147d66c6612d752e98f5fa5f98b4b8fadce6
Bartek <cetlinskib@gmail.com>
2019-11-19 21:22:34 +0100

    Mongo database change to remote.

--- launcher/src/__init__.py
+++ launcher/src/__init__.py
@@ -2,9 +2,11 @@ from flask import Flask
 from flask_mongoalchemy import MongoAlchemy
 
 app = Flask(__name__)
-app.config['MONGOALCHEMY_DATABASE'] = 'CTDatabase' #'mongodb://127.0.0.1:27017/data/db/CTDatabase'
-#app.config['MONGOALCHEMY_SERVER'] = 'mongo'
-#app.config['MONGOALCHEMY_PORT'] = '27017'
+app.config['MONGOALCHEMY_DATABASE'] = 'CTDatabase'
+app.config['MONGOALCHEMY_USER'] = 'CTDatabaseUser'
+app.config['MONGOALCHEMY_PASSWORD'] = 'plan-b-po'
+app.config['MONGOALCHEMY_PORT'] = 27017
+app.config['MONGOALCHEMY_SERVER'] = 'mongodb://ctcluster-ph4yr.mongodb.net'
 db = MongoAlchemy(app)
 
 from launcher.src import Webapp
