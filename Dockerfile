f4495c89ba803afb67ff272860273a688bc9b201
Bartek <cetlinskib@gmail.com>
2019-11-20 00:25:29 +0100

    Update of requirements. Docker files update.

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM tiangolo/uwsgi-nginx-flask:python3.6-alpine3.7
 ENV STATIC_URL /src/templates
 ENV FLASK_APP launcher.py
 WORKDIR /launcher
-COPY ./launcher /launcher
+COPY ./ /launcher
 RUN pip install --trusted-host pypi.python.orh -r requirements.txt
-EXPOSE 80
-CMD ["flask", "run"]
+EXPOSE 5001
+CMD ["python", "launcher/launcher.py"]
--- launcher/src/__init__.py
+++ launcher/src/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from pymongo import MongoClient
 
 app = Flask(__name__)
-db = MongoClient("mongodb+srv://CTDatabaseUser:plan-b-po@ctcluster-ph4yr.mongodb.net/CTDatabase")
+db = MongoClient("mongodb://CTDatabaseUser:plan-b-po@ctcluster-ph4yr.mongodb.net/CTDatabase")
 #app.config['MONGOALCHEMY_DATABASE'] = 'CTDatabase'
 #app.config['MONGOALCHEMY_USER'] = 'CTDatabaseUser'
 #app.config['MONGOALCHEMY_PASSWORD'] = 'plan-b-po'
