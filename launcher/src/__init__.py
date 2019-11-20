490c1ef5ea08ffef226181bb1198d10876b9802d
Bartek <cetlinskib@gmail.com>
2019-11-20 09:16:13 +0100

    Update for db connection initialization.

--- launcher/src/__init__.py
+++ launcher/src/__init__.py
@@ -2,7 +2,12 @@ from flask import Flask
 from pymongo import MongoClient
 
 app = Flask(__name__)
-db = MongoClient("mongodb+srv://CTDatabaseUser:plan-b-po@ctcluster-ph4yr.mongodb.net/CTDatabase")
+db = MongoClient("mongodb+srv://ctcluster-ph4yr.mongodb.net/CTDatabase",
+                 username='CTDatabaseUser',
+                 password='plan-b-po',
+                 authSource='the_database',
+                 authMechanism='SCRAM-SHA-256'
+                 )
 #app.config['MONGOALCHEMY_DATABASE'] = 'CTDatabase'
 #app.config['MONGOALCHEMY_USER'] = 'CTDatabaseUser'
 #app.config['MONGOALCHEMY_PASSWORD'] = 'plan-b-po'
