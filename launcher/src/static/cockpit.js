8de0bb8a5519ff493bb9e1891fe6ba497fe1749e
Bartek <cetlinskib@gmail.com>
2020-01-21 23:45:59 +0100

    JS code cleanup

--- launcher/src/static/cockpit.js
+++ launcher/src/static/cockpit.js
@@ -28,19 +28,19 @@ document.addEventListener('DOMContentLoaded', checkLogin, false);
 function getTaskStatus(task_id) {
     if(task_id) {
         let statusRequest = new XMLHttpRequest;
-        statusRequest.responseType = 'json'
+        statusRequest.responseType = 'json';
         let url = 'https://enigmatic-hollows-51365.herokuapp.com/machine-manager/launcher/computations/'.concat(task_id);
         statusRequest.open('GET', url, true);
         statusRequest.onload = function() {
-            let resp = statusRequest.response
-            console.log(resp)
+            let resp = statusRequest.response;
+            console.log(resp);
             let statusTextID = 'statusText_'.concat(task_id);
             let statusTextDetailsID = 'statusTextDetails_'.concat(task_id);
             let statusText = document.getElementById(statusTextID);
             let statusTextDetails = document.getElementById(statusTextDetailsID);
             statusText.textContent = resp['status'] == null ? "unknown" : resp['status'];
             statusTextDetails.textContent = resp['status'] == null ? "unknown" : resp['status'];
-        }
+        };
     statusRequest.send();
     }
 }
